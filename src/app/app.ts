fe17481a4fa5e8b586b9e05a1263e6e42aa6bc5e
daviano <daviano.amaru@epipoli.com>
2026-01-07 15:57:47 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatFormField],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
